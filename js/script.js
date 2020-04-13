2a31007ece02e98541a313b30b1717aeda0248a5
OseeMukaya <oseemukaya@gmail.com>
2020-04-13 14:07:42 +0200

    validation des informations

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-const btn = document.querySelector('#btn');
-
+const form = document.querySelector('#form');
 const nom = document.querySelector('#nom');
 const prenom = document.querySelector('#prenom');
 const mail = document.querySelector('#mail');
@@ -12,6 +11,17 @@ const pays = document.querySelector('#pays');
 const table = document.querySelector('table');
 const tbody = document.querySelector('tbody');
 
+const errorNom = document.querySelector('.errorNom');
+const errorPrenom = document.querySelector('.errorPrenom');
+const errorMail = document.querySelector('.errorMmail');
+const errorPoste = document.querySelector('.errorPoste');
+const errorTelephone = document.querySelector('.telephone');
+const errorStatut = document.querySelector('.errorStatut');
+const errorPays = document.querySelector('.errorPays');
+
+const inputs = document.querySelectorAll('input');
+const spans = document.querySelectorAll('span');
+
 const employees = [];
 
 /**
@@ -21,12 +31,21 @@ const employees = [];
 const genererUnMatricule = ()=>{
     return  Math.round(Math.random() * 1000000000);
 };
-if(!nom.value.length){
 
+const videLesChampsDuFormulaire = ()=>{
+    nom.value = '',
+    prenom.value = '',
+    mail.value = '',
+    age.value = '',
+    poste.value = '',
+    telephone.value = '',
+    statut.value = '',
+    pays.value = ''
 }
-btn.addEventListener('click',(event)=>{
-    tbody.innerHTML= '';
+
+form.addEventListener('submit',(event)=>{
     event.preventDefault();
+    tbody.innerHTML = '';
     let dataEmployees = {
         matricule: genererUnMatricule(),
         nom: nom.value,
@@ -48,4 +67,23 @@ btn.addEventListener('click',(event)=>{
             td.append(employe[employeEnCour]);
         }
     }
+    videLesChampsDuFormulaire();
+    
+    // event.preventDefault();
+    // if(inputs.length > 0){
+    //     for(let i = 0; i < inputs.length; i++){
+    //         if(inputs[i].value === ''){
+    //             spans.forEach((span)=>{
+    //                 span.textContent = "Champs vide";
+    //                 span.classList.add('error');
+    //             });
+    //         }
+    //     }
+    // }
+    // if(inputs.length <= 0){
+    //     console.log('bonjour mon ami');
+    // }
+
 });
+
+
