5ef47983e1e6457358841ab8dac6f1b8ee28fd49
OseeMukaya <oseemukaya@gmail.com>
2020-04-18 13:10:33 +0200

    validation formulaire terminer

--- js/script.js
+++ js/script.js
@@ -1,51 +1,67 @@
-const form = document.querySelector('#form');
-const nom = document.querySelector('#nom');
-const prenom = document.querySelector('#prenom');
-const mail = document.querySelector('#mail');
-const age = document.querySelector('#age');
-const poste = document.querySelector('#poste');
-const telephone = document.querySelector('#telephone');
-const statut = document.querySelector('#statut');
-const pays = document.querySelector('#pays');
-
-const table = document.querySelector('table');
-const tbody = document.querySelector('tbody');
-
-const errorNom = document.querySelector('.errorNom');
-const errorPrenom = document.querySelector('.errorPrenom');
-const errorMail = document.querySelector('.errorMmail');
-const errorPoste = document.querySelector('.errorPoste');
-const errorTelephone = document.querySelector('.telephone');
-const errorStatut = document.querySelector('.errorStatut');
-const errorPays = document.querySelector('.errorPays');
-
-const inputs = document.querySelectorAll('input');
-const spans = document.querySelectorAll('span');
+const nom = document.querySelector("#nom");
+const prenom = document.querySelector("#prenom");
+const mail = document.querySelector("#mail");
+const age = document.querySelector("#age");
+const poste = document.querySelector("#poste");
+const telephone = document.querySelector("#telephone");
+const statut = document.querySelector("#statut");
+const pays = document.querySelector("#pays");
+const btn = document.querySelector("#btn");
+const table = document.querySelector("table");
+const tbody = document.querySelector("tbody");
 
 const employees = [];
 
-/**
- * genererUnMatricule permet de generer un matricule unique pour chaque employe
- * @return {Number} de 0 à 1000000000
- */
-const genererUnMatricule = ()=>{
-    return  Math.round(Math.random() * 1000000000);
+const genererUnMatricule = () => {
+  return Math.round(Math.random().toString() * 1000000000);
 };
 
-const videLesChampsDuFormulaire = ()=>{
-    nom.value = '',
-    prenom.value = '',
-    mail.value = '',
-    age.value = '',
-    poste.value = '',
-    telephone.value = '',
-    statut.value = '',
-    pays.value = ''
+const videLesChampsDuFormulaire = () => {
+  (nom.value = ""),
+    (prenom.value = ""),
+    (mail.value = ""),
+    (age.value = ""),
+    (poste.value = ""),
+    (telephone.value = ""),
+    (statut.value = ""),
+    (pays.value = "");
+};
+function addTagSpanError(name) {
+  let span = document.createElement("span");
+    span.classList.add('span');
+    name.parentElement.appendChild(span);
+    span = document.querySelector('.span');
+    if(span.textContent === ""){
+        span.textContent = "Champ vide";
+        span.classList.add('error');       
+    }
 }
-
-form.addEventListener('submit',(event)=>{
+function insertValueInTable(event) {
+  if (nom.value === "") {
+    event.preventDefault();
+    addTagSpanError(nom);
+  } else if (prenom.value === "") {
+    event.preventDefault();
+    addTagSpanError(prenom);
+  } else if (mail.value === "") {
+    event.preventDefault();
+    addTagSpanError(mail);
+  } else if (poste.value === "") {
+    event.preventDefault();
+    addTagSpanError(poste);
+  } else if (telephone.value === "") {
+    event.preventDefault();
+    addTagSpanError(telephone);
+  } else if (statut.value === "") {
+    event.preventDefault();
+    addTagSpanError(statut);
+  } else if (pays.value === "") {
+    event.preventDefault();
+    addTagSpanError(pays);
+  } else {
     event.preventDefault();
     tbody.innerHTML = '';
+   
     let dataEmployees = {
         matricule: genererUnMatricule(),
         nom: nom.value,
@@ -58,6 +74,7 @@ form.addEventListener('submit',(event)=>{
         pays: pays.value,
     }
     employees.push(dataEmployees);
+
     for(let employe of employees){
         const tr = document.createElement('tr');
         tbody.append(tr);
@@ -67,23 +84,10 @@ form.addEventListener('submit',(event)=>{
             td.append(employe[employeEnCour]);
         }
     }
-    videLesChampsDuFormulaire();
-    
-    // event.preventDefault();
-    // if(inputs.length > 0){
-    //     for(let i = 0; i < inputs.length; i++){
-    //         if(inputs[i].value === ''){
-    //             spans.forEach((span)=>{
-    //                 span.textContent = "Champs vide";
-    //                 span.classList.add('error');
-    //             });
-    //         }
-    //     }
-    // }
-    // if(inputs.length <= 0){
-    //     console.log('bonjour mon ami');
-    // }
 
-});
+    videLesChampsDuFormulaire();
+  }
+};
+btn.addEventListener("click", insertValueInTable);
 
 
