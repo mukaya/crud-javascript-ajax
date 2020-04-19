94e21b1990cff2bd59e48cb32cfae620f9e154ab
OseeMukaya <oseemukaya@gmail.com>
2020-04-19 06:11:44 +0200

    request avec get reussit

--- js/script.js
+++ js/script.js
@@ -1,28 +1,13 @@
 const nom = document.querySelector("#nom");
 const prenom = document.querySelector("#prenom");
 const mail = document.querySelector("#mail");
-const age = document.querySelector("#age");
 const poste = document.querySelector("#poste");
-const telephone = document.querySelector("#telephone");
 const statut = document.querySelector("#statut");
 const pays = document.querySelector("#pays");
 
 const btn = document.querySelector("#btn");
 const table = document.querySelector("table");
 const tbody = document.querySelector("tbody");
-
-let selectedRow = null;
-
-function onFormSubmit() {
-      let formData = readFormData();
-      if (selectedRow == null){
-          insertNewRecord(formData);
-          resetForm();
-      }else{
-         updateRecord(formData);
-         resetForm();
-      }
-}
 
 const genererUnMatricule = () => {
   return Math.round(Math.random().toString() * 1000000000);
@@ -50,20 +35,53 @@ function addTagSpanError(name) {
         span.classList.add('error');       
     }
 }
+
 function readFormData() {
-  let formData = {};
-  formData["matricule"] = genererUnMatricule();
-  formData["nom"] = nom.value;
-  formData["prenom"] = prenom.value;
-  formData["mail"] = mail.value;
-  formData["age"] = age.value;
-  formData["poste"] = poste.value;
-  formData["telephone"] = telephone.value;
-  formData["statut"] = statut.value;
-  formData["pays"] = pays.value;
-  return formData;
+   tbody.innerHTML = "";
+   axios.get('http://167.71.45.243:4000/api/employes?api_key=ulkzedq')
+  .then((response)=>{
+    datas = response.data;
+    for(let i = 0; i < datas.length; i++){
+      let tr = `
+      <tr>
+        <td>${datas[i]._id}</td>
+        <td>${datas[i].nom}</td>
+        <td>${datas[i].prenom}</td>
+        <td>${datas[i].estMarie ? 'OUI' : 'NOM' }</td>
+        <td>${datas[i].pays}</td>
+        <td>${datas[i].email}</td>
+        <td>${datas[i].poste}</td>
+        <td>
+          <button class="btn btn-primary" id="edit-${datas[i]._id}">Modifier</button>
+          <button class="btn btn-danger" id="delete-${datas[i]._id}">Supprimer</button>
+        </td>
+      </tr>
+      `;
+      tbody.insertAdjacentHTML('beforeend', tr);
+
+      const detete = document.getElementById(`delete-${datas[i]._id}`);
+      detete.addEventListener('click',(event)=>{
+        event.preventDefault();
+        const confirmation = confirm('Tu est sur de supprimer cette enregistrement');
+        if(confirmation){
+          event.preventDefault();
+          axios.delete(`http://167.71.45.243:4000/api/employes?api_key=ulkzedq/${datas[i]._id}}`)
+          .then(res => {
+            console.log(res.data)
+          })
+          .catch(err => {
+            console.error(err); 
+          })
+        }
+      });
+    }
+   })
+  .catch((error)=>{
+    console.log(error);
+  })
 }
-function insertNewRecord(data){
+readFormData();
+function insertNewRecord(){
   if (nom.value === "") {
     addTagSpanError(nom);
   } else if (prenom.value === "") {    
@@ -72,37 +90,31 @@ function insertNewRecord(data){
     addTagSpanError(mail);
   } else if (poste.value === "") {     
     addTagSpanError(poste);
-  } else if (telephone.value === "") {     
-    addTagSpanError(telephone);
-  } else if (statut.value === "") {     
+  }else if (statut.value === "") {     
     addTagSpanError(statut);
   } else if (pays.value === "") {     
     addTagSpanError(pays);
   } else {
-    let table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
-    let newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.matricule;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.nom;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.prenom;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.mail;
-    cell5 = newRow.insertCell(4);
-    cell5.innerHTML = data.age;
-    cell6 = newRow.insertCell(5);
-    cell6.innerHTML = data.poste;
-    cell7 = newRow.insertCell(6);
-    cell7.innerHTML = data.telephone;
-    cell8 = newRow.insertCell(7);
-    cell8.innerHTML = data.statut;
-    cell9 = newRow.insertCell(8);
-    cell9.innerHTML = data.pays;
-    cell10 = newRow.insertCell(9);
-    cell10.innerHTML = `<a onClick="onEdit(this)" class="btn btn-primary">Modifier</a>
-                       <a onClick="onDelete(this)" class="btn btn-danger">Supprimer</a>`;
+    const data = {
+      //_id: genererUnMatricule(),
+      nom: nom.value,
+      prenom: prenom.value,
+      estMarie: statut.value,
+      pays: pays.value,
+      email: mail.value,
+      poste: poste.value
+    }
+    axios.post('http://167.71.45.243:4000/api/employes?api_key=ulkzedq',data)
+    .then(res => {
+     console.log(res.data);
+    })
+    .catch(err => {
+      console.error(err); 
+    })
   }
+}
+function onFormSubmit(){
+  insertNewRecord();
 }
 function onEdit(td) {
   btn.textContent = "Mettre à jour";
